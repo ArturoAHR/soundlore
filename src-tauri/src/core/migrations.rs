d329fd0bcd9416d3e965b4a85f2861cf8051509d
Arturo Hernández <arturoaureliohr@gmail.com>
2026-04-04 00:06:03 -0400

    feat: add logging to migrations and file utils

--- src-tauri/src/core/migrations.rs
+++ src-tauri/src/core/migrations.rs
@@ -1,11 +1,16 @@
 use std::{error::Error, str::FromStr};
 
 use sqlx::{sqlite::SqliteConnectOptions, SqlitePool};
+use tauri_plugin_log::log::info;
 
 use crate::core::database::get_database_path;
 
 pub async fn run_migrations(app: &tauri::AppHandle) -> Result<(), Box<dyn Error>> {
-    let options = SqliteConnectOptions::from_str(&get_database_path(app))?.create_if_missing(true);
+    let database_path = get_database_path(app);
+
+    info!("Running migrations on database at {:?}", database_path);
+
+    let options = SqliteConnectOptions::from_str(&database_path)?.create_if_missing(true);
     let pool = SqlitePool::connect_with(options).await?;
 
     Ok(sqlx::migrate!().run(&pool).await?)
--- src-tauri/src/file/utils.rs
+++ src-tauri/src/file/utils.rs
@@ -1,19 +1,24 @@
 use std::path::{Path, PathBuf};
 
+use tauri_plugin_log::log::{debug, trace};
 use walkdir::WalkDir;
 
-pub fn find_music_files(root: &Path) -> Vec<PathBuf> {
+pub fn find_track_files(root: &Path) -> Vec<PathBuf> {
+    debug!("Finding music files in {:?}", root);
+
     WalkDir::new(root)
         .follow_links(true)
         .into_iter()
         .filter_map(|e| e.ok())
         .filter(|e| e.file_type().is_file())
-        .filter(|e| is_supported_music_file(e.path()))
+        .filter(|e| is_supported_track_file(e.path()))
         .map(|e| e.path().to_owned())
         .collect()
 }
 
-pub fn is_supported_music_file(path: &Path) -> bool {
+pub fn is_supported_track_file(path: &Path) -> bool {
+    trace!("Checking if {:?} is a music file", path);
+
     matches!(
         path.extension().and_then(|e| e.to_str()),
         Some("mp3" | "flac" | "ogg" | "opus" | "m4a" | "aac" | "wav" | "aiff")
