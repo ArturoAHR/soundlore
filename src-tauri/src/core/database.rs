8fb3d6b154d6b53a4f2548d1b9bf65b8a386be1e
Arturo Hernández <arturoaureliohr@gmail.com>
2026-04-04 01:54:48 -0400

    feat: add logging in initial app database setup

--- src-tauri/src/core/database.rs
+++ src-tauri/src/core/database.rs
@@ -2,6 +2,7 @@ use std::str::FromStr;
 
 use sqlx::{sqlite::SqliteConnectOptions, SqlitePool};
 use tauri::Manager;
+use tauri_plugin_log::log::debug;
 
 use crate::{config::DATABASE_FILE_NAME, error::AppError};
 
@@ -16,8 +17,15 @@ pub fn get_database_path(app: &tauri::AppHandle) -> String {
 
 pub async fn create_pool(app: &tauri::AppHandle) -> Result<SqlitePool, AppError> {
     let database_path = get_database_path(app);
+
+    debug!("Connecting to database in location: {}", database_path);
+
     let options = SqliteConnectOptions::from_str(&database_path)?.create_if_missing(true);
-    Ok(SqlitePool::connect_with(options).await?)
+    let pool = SqlitePool::connect_with(options).await?;
+
+    debug!("Successfully connected to database.");
+
+    Ok(pool)
 }
 
 /// Verifies the current schema version and determines if migrations are needed
@@ -41,13 +49,19 @@ pub async fn check_schema_version(pool: &SqlitePool) -> Result<bool, AppError> {
 pub fn get_expected_schema_version() -> i64 {
     let migrator = sqlx::migrate!();
 
-    migrator.migrations.len() as i64
+    let version = migrator.migrations.len() as i64;
+
+    debug!("Expected Schema Version: {}", version);
+
+    version
 }
 
 pub async fn get_current_schema_version(pool: &SqlitePool) -> Result<i64, AppError> {
     let version = sqlx::query_scalar::<_, i64>("PRAGMA user_version")
         .fetch_one(pool)
         .await?;
+
+    debug!("Current Schema Version: {}", version);
 
     Ok(version)
 }
--- src-tauri/src/core/migrations.rs
+++ src-tauri/src/core/migrations.rs
@@ -1,8 +1,11 @@
 use std::error::Error;
 
 use sqlx::SqlitePool;
+use tauri_plugin_log::log::info;
 
 pub async fn run_migrations(pool: &SqlitePool) -> Result<(), Box<dyn Error>> {
+    info!("Running database migrations.");
+
     sqlx::migrate!().run(pool).await?;
 
     Ok(())
