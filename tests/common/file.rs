9abf531fdbce2873d1f5b2ae3a4c047645a70768
Arturo Hernández <arturoaureliohr@gmail.com>
2026-05-23 15:33:11 -0400

    feat: add integration tests common helpers module

---
+++ tests/common/file.rs
@@ -0,0 +1,46 @@
+use std::{
+    env,
+    fs::create_dir_all,
+    hash::{DefaultHasher, Hash, Hasher},
+    path::{Path, PathBuf},
+    sync::LazyLock,
+};
+
+use crate::common::generator::generate_audio_file_fixtures;
+
+// TODO: Add recursive path where there are audio files in sub-folders when recursive scan is implemented.
+pub struct AudioFileFixturesPath {
+    pub all_formats: PathBuf,
+    pub metadata_variants: PathBuf,
+    pub corrupt: PathBuf,
+    pub partially_corrupt: PathBuf,
+}
+
+pub static AUDIO_FILE_FIXTURES_PATH: LazyLock<AudioFileFixturesPath> = LazyLock::new(|| {
+    let fixture_root = get_fixture_root();
+
+    if !fixture_root.exists() {
+        create_dir_all(&fixture_root).expect("Couldn't create audio files fixture directory");
+
+        generate_audio_file_fixtures(&fixture_root);
+    }
+
+    todo!()
+});
+
+/// Hash based off the generator file source code, to ensure that no stale fixtures are used
+pub static FIXTURE_HASH: LazyLock<String> = LazyLock::new(|| {
+    let generator_source_code = include_str!("./generator.rs");
+
+    let mut hasher = DefaultHasher::new();
+    generator_source_code.hash(&mut hasher);
+
+    format!("{:016x}", hasher.finish())
+});
+
+fn get_fixture_root() -> PathBuf {
+    let base = env::var("CARGO_TARGET_TMPDIR")
+        .expect("CARGO_TARGET_TMPDIR environment variable is not set");
+
+    Path::new(&base).join("audio").join(&*FIXTURE_HASH)
+}
---
+++ tests/common/generator.rs
@@ -0,0 +1,5 @@
+/* WARNING: Changes to this file will result in the regeneration of the fixtures on the next test run */
+
+use std::path::PathBuf;
+
+pub fn generate_audio_file_fixtures(path: &PathBuf) {}
