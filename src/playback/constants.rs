b65489784bd1ae1bc2c2d2fd6a691ee95b837c34
Arturo Hernández <arturoaureliohr@gmail.com>
2026-05-31 00:07:29 -0400

    refactor: move resampler constants to the correct file

--- src/playback/constants.rs
+++ src/playback/constants.rs
@@ -1,1 +1,11 @@
 pub const SAMPLE_BUFFER_CAPACITY: usize = 19200;
+
+/*
+ * Ratio between chunk size and sub chunk size determines latency and quality, a higher ratio increases
+ * quality and also latency, a lower ratio reduces quality but increases latency, recommended ratio is
+ * 100 to 1000.
+ *
+ * Currently we are optimizing for highest quality.
+ */
+pub static RESAMPLER_CHUNK_SIZE: usize = 2048;
+pub static RESAMPLER_SUB_CHUNK_SIZE: usize = 2;
--- src/playback/pipeline/resampler.rs
+++ src/playback/pipeline/resampler.rs
@@ -8,17 +8,8 @@ use rubato::{
 use thiserror::Error;
 use tracing::{info, instrument};
 
+use crate::playback::constants::{RESAMPLER_CHUNK_SIZE, RESAMPLER_SUB_CHUNK_SIZE};
 use crate::playback::pipeline::AudioFormat;
-
-/*
- * Ratio between chunk size and sub chunk size determines latency and quality, a higher ratio increases
- * quality and also latency, a lower ratio reduces quality but increases latency, recommended ratio is
- * 100 to 1000.
- *
- * Currently we are optimizing for highest quality.
- */
-pub static RESAMPLER_CHUNK_SIZE: usize = 2048;
-pub static RESAMPLER_SUB_CHUNK_SIZE: usize = 2;
 
 #[derive(Debug, Error, Clone)]
 pub enum AudioResamplerError {
