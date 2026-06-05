1548fd81fa3b0261a40a3b9baedbe312c5157ba8
Arturo Hernández <arturoaureliohr@gmail.com>
2026-06-04 23:21:06 -0400

    test: add test case for different output sample rates and channel counts

--- tests/app/playback/mod.rs
+++ tests/app/playback/mod.rs
@@ -1,6 +1,7 @@
 use std::{thread, time::Duration};
 
 use nameless_music_player_lib::playback::pipeline::event_emitter::AudioPipelineEvent;
+use tracing::debug;
 
 use crate::{
     assert_timeout,
@@ -36,7 +37,7 @@ fn decodes_samples_into_consumer() {
 }
 
 #[test]
-fn decodes_different_sample_rates_and_channels() {
+fn decodes_different_sample_rates_and_channels_with_44100_stereo_output() {
     let output_sample_rate = 44100;
     let output_channels = 2;
 
@@ -56,12 +57,10 @@ fn decodes_different_sample_rates_and_channels() {
                 let channel_count_name = match channels {
                     1 => "mono",
                     2 => "stereo",
-                    _ => unimplemented!(),
+                    _ => unreachable!(),
                 };
 
                 let file_name = format!("{}_{}.{}", sample_rate, channel_count_name, format);
-
-                println!("{file_name}");
 
                 playback
                     .playback_controller
@@ -100,13 +99,86 @@ fn decodes_different_sample_rates_and_channels() {
                     let _ = sample_buffer_consumer.pop_entire_slice(&mut output_samples);
                 }
 
-                println!("Sample count for {file_name}: {sample_count}");
+                debug!("Sample count for {file_name}: {sample_count}");
 
-                // assert_eq!(
-                //     sample_count,
-                //     output_channels as usize * output_sample_rate as usize,
-                //     "Sample count mismatch for file: {file_name}"
-                // )
+                assert!(
+                    sample_count >= output_channels as usize * output_sample_rate as usize,
+                    "Insufficient sample count for file: {file_name}",
+                );
+            }
+        }
+    }
+}
+
+#[test]
+fn decodes_different_sample_rates_and_channels_with_48000_stereo_output() {
+    let output_sample_rate = 48000;
+    let output_channels = 2;
+
+    let mut playback = TestPlayback::build(output_sample_rate, output_channels);
+
+    let audio_file_fixtures_path = &*AUDIO_FILE_FIXTURES_PATH;
+
+    let sample_rates = vec![48000, 44100];
+    let channel_counts = vec![1, 2];
+    let formats = vec!["wav", "mp3", "ogg", "aac", "flac", "m4a", "aiff"];
+
+    for sample_rate in sample_rates.iter() {
+        for channels in channel_counts.iter() {
+            for format in formats.iter() {
+                let mut sample_count = 0;
+
+                let channel_count_name = match channels {
+                    1 => "mono",
+                    2 => "stereo",
+                    _ => unreachable!(),
+                };
+
+                let file_name = format!("{}_{}.{}", sample_rate, channel_count_name, format);
+
+                playback
+                    .playback_controller
+                    .play(Some(
+                        audio_file_fixtures_path
+                            .all_sample_rates_and_channels
+                            .join(&file_name),
+                    ))
+                    .unwrap();
+
+                let mut playback_engine = playback.playback_engine.borrow_mut();
+
+                loop {
+                    let sample_buffer_consumer =
+                        playback_engine.sample_buffer_consumer.as_mut().unwrap();
+
+                    if sample_buffer_consumer.is_empty() {
+                        if let Ok(event) = playback.playback_controller.poll_audio_pipeline_event()
+                        {
+                            match event {
+                                Some(AudioPipelineEvent::EndOfTrack) => break,
+                                _ => {
+                                    thread::sleep(Duration::from_millis(100));
+                                    continue;
+                                }
+                            }
+                        }
+                    }
+
+                    let samples_contained = sample_buffer_consumer.slots();
+
+                    let mut output_samples = vec![0.0; samples_contained];
+
+                    sample_count += samples_contained;
+
+                    let _ = sample_buffer_consumer.pop_entire_slice(&mut output_samples);
+                }
+
+                debug!("Sample count for {file_name}: {sample_count}");
+
+                assert!(
+                    sample_count >= output_channels as usize * output_sample_rate as usize,
+                    "Insufficient sample count for file: {file_name}",
+                );
             }
         }
     }
